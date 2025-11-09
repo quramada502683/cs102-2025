9cf0940c112a4bcb250b1b99b9967b124d1136e5
User <makassimm@gmail.com>
2025-11-09 19:42:48 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -11,7 +11,19 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for char in plaintext:
+        if not char.isalpha():
+            ciphertext += char
+            continue
+
+        if char.islower():
+            x = ord(char)
+            new_x = (((x - 97) + shift) % 26) + 97
+            ciphertext += chr(new_x)
+        else:
+            x = ord(char)
+            new_x = (((x - 65) + shift) % 26) + 65
+            ciphertext += chr(new_x)
     return ciphertext
 
 
@@ -28,5 +40,17 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for char in ciphertext:
+        if not char.isalpha():
+            plaintext += char
+            continue
+
+        if char.islower():
+            x = ord(char)
+            new_x = (((x - 97) - shift) % 26) + 97
+            plaintext += chr(new_x)
+        else:
+            x = ord(char)
+            new_x = (((x - 65) - shift) % 26) + 65
+            plaintext += chr(new_x)
     return plaintext

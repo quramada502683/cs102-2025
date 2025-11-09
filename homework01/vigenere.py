3591942b4128e3fd5f37a98fc7c9b1952797eaec
User <makassimm@gmail.com>
2025-11-10 00:29:37 +0300

    Format code with Black

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -14,12 +14,12 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
 
     for char in plaintext:
         if char.isalpha():
-            shift = ord(keyword[key_index % len(keyword)]) - ord('A')
+            shift = ord(keyword[key_index % len(keyword)]) - ord("A")
 
             if char.isupper():
-                ciphertext += chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+                ciphertext += chr((ord(char) - ord("A") + shift) % 26 + ord("A"))
             else:
-                ciphertext += chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+                ciphertext += chr((ord(char) - ord("a") + shift) % 26 + ord("a"))
 
             key_index += 1
         else:
@@ -45,12 +45,12 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
 
     for char in ciphertext:
         if char.isalpha():
-            shift = ord(keyword[key_index % len(keyword)]) - ord('A')
+            shift = ord(keyword[key_index % len(keyword)]) - ord("A")
 
             if char.isupper():
-                plaintext += chr((ord(char) - ord('A') - shift) % 26 + ord('A'))
+                plaintext += chr((ord(char) - ord("A") - shift) % 26 + ord("A"))
             else:
-                plaintext += chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
+                plaintext += chr((ord(char) - ord("a") - shift) % 26 + ord("a"))
 
             key_index += 1
         else:

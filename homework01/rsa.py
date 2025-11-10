78ca39d3810e012a0f9555b2e9a13dd4f81339b3
User <makassimm@gmail.com>
2025-11-10 18:04:11 +0300

    Format code with black

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -19,7 +19,7 @@ def is_prime(n: int) -> bool:
     if n % 2 == 0:
         return False
 
-    for i in range(3, int(n ** 0.5) + 1, 2):
+    for i in range(3, int(n**0.5) + 1, 2):
         if n % i == 0:
             return False
 
@@ -78,7 +78,6 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
     # phi = (p-1)(q-1)
     phi = (p - 1) * (q - 1)
 
-
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
 
@@ -110,7 +109,7 @@ def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
     # Unpack the key into its components
     key, n = pk
     # Generate the plaintext based on the ciphertext and key using a^b mod m
-    plain = [chr((char ** key) % n) for char in ciphertext]
+    plain = [chr((char**key) % n) for char in ciphertext]
     # Return the array of bytes as a string
     return "".join(plain)
 

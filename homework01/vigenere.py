26e517419511b7131abdd0695c3d9263fc6bb5c7
User <makassimm@gmail.com>
2025-11-10 18:04:24 +0300

    Format code with black

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -28,7 +28,6 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     return ciphertext
 
 
-
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.

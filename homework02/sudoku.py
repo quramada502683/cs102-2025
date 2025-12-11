734cc8569519e5dff89b8b5da5fef96f452cea7c
User <makassimm@gmail.com>
2025-12-11 21:22:37 +0300

    group

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -41,6 +41,11 @@ def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
+    if len(values) % n != 0:
+        raise ValueError("Длина списка не кратна n")
+
+    result = [values[i: i + n] for i in range(0, len(values), n)]
+    return result
     pass
 
 

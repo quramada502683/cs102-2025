5067fe8bddfd659a7368b81d89dac4c10ad20cf2
User <makassimm@gmail.com>
2025-12-15 18:09:04 +0300

    draw_borders

--- homework04/life_console.py
+++ homework04/life_console.py
@@ -10,7 +10,16 @@ class Console(UI):
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+        screen.addch(0, 0, "+")
+        screen.addch(0, self.life.cols + 1, "+")
+        screen.addch(self.life.rows + 1, 0, "+")
+        screen.addch(self.life.rows + 1, self.life.cols + 1, "+")
+        for x in range(1, self.life.cols + 1):
+            screen.addch(0, x, "-")
+            screen.addch(self.life.rows + 1, x, "-")
+        for y in range(1, self.life.rows + 1):
+            screen.addch(y, 0, "|")
+            screen.addch(y, self.life.cols + 1, "|")
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
